f37eefda9b29a669ab3815c68b2a2a9621ee0c89
mortalshot <seregagl1996@gmail.com>
2021-01-15 21:02:33 +0300

    project init. header complete and first screen on main page

---
+++ #src/js/components/slider.js
@@ -0,0 +1,41 @@
+var $status = $('.page-info');
+var $slickElement = $('.main__items');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.html('<span class="page-info__current">' + i + '</span>' + '<span class="page-info__all"> / ' + slick.slideCount + '</span>');
+});
+
+$slickElement.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="icon-arrow-banner-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icon-arrow-banner-right"></i></button>',
+});
+
+// $('.gallery__thumbnails').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.gallery__main',
+//     arrows: false,
+//     focusOnSelect: true,
+//     vertical: true,
+//     verticalSwiping: true,
+
+//     responsive: [
+//         {
+//           breakpoint: 991,
+//           settings: {
+//             vertical: false,
+//             verticalSwiping: false,
+//           }
+//         },
+//       ]
+// });
+
+// $('#productGallery .tabs-triggers__item').click(function() {
+//     $('.gallery__main').slick('refresh');
+//     $('.gallery__thumbnails').slick('refresh');
+// })
---
+++ #src/js/index.js
@@ -0,0 +1,8 @@
+// libs
+@@include('libs/dynamic-adapt.js')
+
+// components
+@@include('components/webp.js')
+@@include('components/menu.js')
+@@include('components/slider.js')
+
